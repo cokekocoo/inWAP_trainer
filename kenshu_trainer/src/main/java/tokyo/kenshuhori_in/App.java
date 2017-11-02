fc1364489d1140dc51dc634816dda2801a2ecea6
Kenshu Hori <hori_ke@worksap.co.jp>
2017-11-02 15:31:51 +0900

    change projectDescription
    プロジェクトの名称を変更しました

--- kenshu_trainer/src/main/java/tokyo/kenshuhori_in/App.java
+++ kenshu_trainer/src/main/java/tokyo/kenshuhori_in/App.java
@@ -1,9 +1,7 @@
 package tokyo.kenshuhori_in;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
