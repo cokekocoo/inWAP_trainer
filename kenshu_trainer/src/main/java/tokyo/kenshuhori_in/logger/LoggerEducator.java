fbab7acd26d7e95fc636dbcc50144a1fbd3fa5e0
Kenshu Hori <hori_ke@worksap.co.jp>
2017-11-02 15:35:30 +0900

    create logger package
    Loggerを出力するクラスを生成しました

---
+++ kenshu_trainer/src/main/java/tokyo/kenshuhori_in/logger/LoggerEducator.java
@@ -0,0 +1,39 @@
+package tokyo.kenshuhori_in.logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.StreamHandler;
+
+public class LoggerEducator {
+	private static Logger logger;
+	private static ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    private static StreamHandler streamHandler;
+
+	public void loggerMain() {
+		setUp();
+		outputLogger();
+	}
+
+	private void setUp() {
+		logger = Logger.getLogger("LoggerEducator");
+		try {
+            FileHandler fileHandler = new FileHandler("C:\\COMPANY_UPDATER\\test\\loggerDcucation.log");
+            fileHandler.setLevel(Level.INFO);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+            streamHandler = new StreamHandler(byteArrayOutputStream, new SimpleFormatter());
+            streamHandler.setLevel(Level.SEVERE);
+            logger.addHandler(streamHandler);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Fail init logger to file", e);
+        }
+	}
+
+	private void outputLogger() {
+		logger.log(Level.INFO, "message output succeed");
+	}
+}
