48de642437577cd132a9bef3771ae029e26f8a07
Kenshu Hori <hori_ke@worksap.co.jp>
2017-11-29 17:52:28 +0900

    add thread test
    forEach(Thread :: start)の挙動を見てみたくてソースコードを追加した

--- kenshu_trainer/src/main/java/tokyo/kenshuhori_in/App.java
+++ kenshu_trainer/src/main/java/tokyo/kenshuhori_in/App.java
@@ -2,12 +2,12 @@ package tokyo.kenshuhori_in;
 
 import java.io.IOException;
 
-import tokyo.kenshuhori_in.fileSpace.FileSpaceMain;
+import tokyo.kenshuhori_in.threads.ThreadMain;
 
 public class App {
     public static void main( String[] args ) throws IOException {
 
-    	new FileSpaceMain().execute();
+    	new ThreadMain().execute();
 
     }
 }
